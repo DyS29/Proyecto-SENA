f15796977d5b73bc484e4795d876f3684ed1be2f
DyS29 <deparra12@soy.sena.edu.com>
2024-05-25 21:01:31 -0500

    PROYECTO REACT

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main class="container">
+        <h1> Inicio de sesion </h1>
+        <form action="http://localhost:3000/" method="post">
+          <input type="number" name="celular" id="" placeholder="N. Celular" />
+          <input type="password" name="contraseña" id="" placeholder="Contraseña" />
+          <button>Ingresar</button>
+        </form>
+        {/* olvido su contraseña */}
+        <div>
+
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
