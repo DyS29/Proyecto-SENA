5f64f74d0298910186abd366b7151c65bde9911f
DyS29 <deparra12@soy.sena.edu.com>
2024-06-25 22:55:05 -0500

    se anexregistro o inicio de sesion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,37 @@ function App() {
   const [celular, setCelular] = useState("")
   const [clave, setClave] = useState("")
   const [logueado, setLogueado] = useState(false)
+  const [registrou, setusuarioRegritro] = useState("") //constante registro dos //
+  const [claver, setClaveRegritro] = useState("")
 
   async function ingresar(evento) {
     evento.preventDefault()
     const peticion = await fetch("http://localhost:3000/login?celular=" + celular + "&clave=" + clave)
     setLogueado(true)
   }
+
+  async function resgitro(evento) {
+    evento.preventDefault()
+    const peticion = await fetch("http://localhost:3000/regitro?celular=" + celular + "&clave=" + clave)
+    setLogueado(true)
+  }
+
   function cambiarCelular(evento) {
     setCelular(evento.target.value)
   }
   function cambiarClave(evento) {
     setClave(evento.target.value)
   }
+
+  function usuarioregistro(evento) {
+    setusuarioRegritro(evento.target.value)
+  }
+  function claveregistro(evento) {
+    setClaveRegritro(evento.target.value)
+  }
+
+
+
   if (logueado) {
     return <Confetti />
   }
@@ -26,7 +45,7 @@ function App() {
     <>
       <main class="container">
         <h1> Inicio de sesion </h1>
-        <form onSubmit={ingresar}     >
+        <form onSubmit={ingresar}>
           <input value={celular} type="number" name="celular" id="" placeholder="N. Celular" onChange={cambiarCelular} />
           <input value={clave} type="password" name="contraseña" id="" placeholder="Contraseña" onChange={cambiarClave} />
           <button>Ingresar</button>
@@ -35,9 +54,9 @@ function App() {
 
         <h1>Registro</h1>
         <div>
-          <form >
-            <input type="number" name="celular" id="" placeholder="N. Celular" />
-            <input type="password" name="contraseña" id="" placeholder="Contraseña" />
+          <form onsumit={resgitro}>
+            <input value={registrou} type="number" name="celular" id="" placeholder="N. Celular" onChange={usuarioregistro} />
+            <input value={claver} type="password" name="contraseña" id="" placeholder="Contraseña" onChange={claveregistro} />
             <button>Registro</button>
           </form>
         </div>
